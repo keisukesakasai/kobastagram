7364251f2856d780b24e5f9aef83bb557a1e3a47
keisukesakasai <k6s4i5.3rx@gmail.com>
2022-04-18 01:48:33 +0900

    :bug: fix minor change

--- lib/main.dart
+++ lib/main.dart
@@ -42,31 +42,28 @@ class _MyHomePageState extends State<MyHomePage> {
         length: 5,
         child: Scaffold(
             appBar: PreferredSize(
-                preferredSize: Size.fromHeight(40),
+                preferredSize: Size.fromHeight(45),
                 child: AppBar(
                   centerTitle: true,
                   backgroundColor: Colors.white,
-                  title: Image.asset('images/kobasutagram.png', height: 60,),
-
+                  title: Image.asset(
+                    'images/kobasutagram.png',
+                    height: 50,),
                    actions: [
                     Icon(Icons.add_box_outlined,color:Colors.black),
                     SizedBox(width: 15,),
                     Icon(Icons.favorite_border_outlined,color:Colors.black),
                     SizedBox(width: 15,), //Iconの間隔
                     Padding(
-                    padding: const EdgeInsets.only(bottom: 5.0),
+                    padding: const EdgeInsets.only(bottom: 6.5),
                     child: Transform.rotate( //Iconの角度変更
-                    angle: -20,
+                    angle: 5.5,
                     child: Icon(Icons.send_outlined,color:Colors.black)
                     ),
                     ),
-                    SizedBox(width: 15,),
+                    SizedBox(width: 10,),
                     ],
                 )),
-
-
-
-
             body: TabBarView(children: [
               //--- １個目のタブ
               SingleChildScrollView(
