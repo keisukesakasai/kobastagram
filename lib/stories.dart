427dca4ba89180b36fd4fefcca34c866582d8126
keisukesakasai <k6s4i5.3rx@gmail.com>
2022-04-20 09:32:37 +0900

    :sparkles: add scrollview to story

--- lib/stories.dart
+++ lib/stories.dart
@@ -4,7 +4,6 @@ import 'package:kobastagram/story/story_item_yuji_dad.dart';
 import 'package:kobastagram/story/story_item_mayu_mom.dart';
 import 'package:kobastagram/story/story_item_mayu_dad.dart';
 
-
 class Stories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,13 +11,19 @@ class Stories extends StatelessWidget {
         decoration: BoxDecoration(
             border: Border(
                 bottom: BorderSide(width: 1.0, color: Color(0x3333000000)))),
-        child: Row(
-          children: <Widget>[
-            StoryItemYujiMom(name: "Yuji mom", icon: Icons.account_circle),
-            StoryItemYujiDad(name: "Yuji dad", icon: Icons.account_circle),
-            StoryItemMayuMom(name: "Mayu mom", icon: Icons.account_circle),
-            StoryItemMayuDad(name: "Mayu dad", icon: Icons.account_circle),
-          ],
-        ));
+        child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: <Widget>[
+                StoryItemYujiMom(name: "Yuji mom", icon: Icons.account_circle),
+                StoryItemYujiDad(name: "Yuji dad", icon: Icons.account_circle),
+                StoryItemMayuMom(name: "Mayu mom", icon: Icons.account_circle),
+                StoryItemMayuDad(name: "Mayu dad", icon: Icons.account_circle),
+                StoryItemMayuDad(name: "Mayu dad", icon: Icons.account_circle),
+                StoryItemMayuDad(name: "Mayu dad", icon: Icons.account_circle),
+                StoryItemMayuDad(name: "Mayu dad", icon: Icons.account_circle),
+              ],
+            )));
   }
 }
