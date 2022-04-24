c1a775635f6276361bff97eb6e7059c6fe3c77af
KenIkeda <90637481+Wedding1106@users.noreply.github.com>
2022-04-25 08:23:56 +0900

    add mayu's profile and story_icon (#9)
    @Wedding1106 
    なんか commit 漏れてない？笑
    PR してくれた資材は問題ないので、マージします−

---
+++ lib/post/mayu_profile/post_item_mayu_junior.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+
+import 'dart:math';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final String LeftImagePath = 'images/yuji_profile_high_icon.jpg';
+final String RightImagePath  = 'images/yuji_profile_junior.jpg';
+final String iconPath = 'images/yuji_introduce_profile_icon.png';
+final String general = '中学時代';
+final String school = '柏市立光中学校';
+final String message1 =  '長距離も短距離も速く';
+final String message2 =  '黄色い声援を浴びていた';
+final String message3=  '勉強もでき、\n''学級で常に上位の成績';
+final String hashtag = '#当時から優男\n'
+    '#負けず嫌い#お調子者';
+
+class PostItemYujiJunior extends StatefulWidget {
+  @override
+  _PostItemState createState() => _PostItemState();
+}
+
+class _PostItemState extends State {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(width: 1, color: Color(0x33330000)))),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20, right: 1, left: 1),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 250,
+                    width: 170,
+                    child: Flexible(
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 10,
+                              ), //間隔
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundImage: AssetImage(
+                                    LeftImagePath),
+                              ),
+
+                              Row(
+                                children: [
+                                  Column(
+                                    // crossAxisAlignment: CrossAxisAlignment.start,//写真の横にusernameを配置
+                                    children: [
+                                      Column(
+                                        children: [
+                                          Text(
+                                            general,
+                                            style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 1, 55, 142),
+                                              fontSize: 15,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ), //profile行
+
+                          Row(
+                            //Icon行
+                            children: [
+                              SizedBox(width: 5),
+                              Text('📍$school'),
+                            ],
+                          ), //Row2
+                          Column(
+                            //Icon行
+                            children: [
+                              Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 1),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        message1,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(
+                                        message2,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(
+                                        message3,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      Text(hashtag,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color:
+                                          Color.fromARGB(255, 1, 55, 142),
+                                        ),
+                                      ),
+                                    ],
+                                  )),
+                              Container(
+                                child: Column(
+                                  children: [
+
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ), //Row3
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 0.5,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 8,
+                                      backgroundImage: AssetImage(
+                                          iconPath), //Story投稿画像
+                                    ),
+                                    Text(
+                                      'blaugrana.reysol',
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: 50,
+                                ),
+                                Icon(Icons.more_horiz, size: 12),
+                              ],
+                            ),
+                            Center(
+                                child: Image.asset(
+                                  RightImagePath,
+                                  fit: BoxFit.cover,
+                                  height: 200,
+                                  width: 165.0,
+                                )),
+                            Container(
+                              child: Row(
+                                //mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(children: [
+                                    Icon(Icons.favorite_outline_outlined,
+                                        size: 12),
+                                    Icon(Icons.mode_comment_outlined,
+                                        size: 12),
+                                    Icon(Icons.send_outlined, size: 12),
+                                    SizedBox(
+                                      width: 95,
+                                    )
+                                  ]),
+                                  Icon(
+                                    Icons.home,
+                                    size: 12,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: 1,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border(
+                      bottom:
+                      BorderSide(width: 1, color: Color(0x33330000)))),
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.fromLTRB(12, 0, 12, 8),
+            ),
+          ],
+        ));
+
+
+  }
+}
---
+++ lib/post/posts_mayu_profile.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/material.dart';
+import 'package:kobastagram/post/yuji_profile/post_item_yuji_junior.dart';
+import 'package:kobastagram/post/yuji_profile/post_item_yuji_high.dart';
+import 'package:kobastagram/post/yuji_profile/post_item_yuji_college.dart';
+import 'package:kobastagram/post/yuji_profile/post_item_yuji_company.dart';
+
+final String iconPath = 'images/yuji_introduce_profile_icon.png';
+final String friendsPath = 'images/yuji_profile_friends.png';
+final String profileMsg = '千葉県柏市出身\n''趣味はサッカー観戦、ゴルフ\n''素敵な奥さんと出会い\n''人生勝ち組';
+class PostYujiProfile extends StatefulWidget {
+  @override
+  _PostsState createState() => _PostsState();
+}
+
+
+class _PostsState extends State<PostYujiProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        Container(
+            child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20, right: 1, left: 1),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  CircleAvatar(
+                                    radius: 45,
+                                    backgroundImage: AssetImage(
+                                        'images/yuji_profile_icon.png'),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ), //profile行
+                    ],
+                  ),
+                  Expanded(
+                    child: Container(
+                        child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text(
+                                '100',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '優しさ',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  //fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              '100',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '運動神経',
+                              style: TextStyle(
+                                fontSize: 13,
+                                //fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text(
+                                '100',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '適応性',
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  //fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Yuji kobayashi',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'テクニシャン',
+                        style: TextStyle(
+                          color: Color(0xE85D5F5D),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Text(profileMsg),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Image.asset(
+                  friendsPath,
+                  fit: BoxFit.cover,
+                  width: 100.0,
+                ),
+                Text(
+                  'フォロワー:',
+                  style: TextStyle(fontSize: 13),
+                ),
+                Flexible(
+                  child: Text(
+                    'saka_kei, kennn405, ekedadadadada, 他',
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.visible,
+                  ),
+                ),
+              ],
+            ), //friends
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 5,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: 30.0,
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: Color(0x33330000),
+                      width: 0.5,
+                    ),
+                  ),
+                  child: Text(
+                    'フォロー中 ↓',
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Color(0xF75FB338),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: 30.0,
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: Color(0x33330000),
+                      width: 0.5,
+                    ),
+                  ),
+                  child: Text(
+                    'メッセージ',
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: 30.0,
+                  width: 30.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: Color(0x33330000),
+                      width: 0.5,
+                    ),
+                  ),
+                  child: Icon(Icons.switch_account_outlined, size: 19),
+                ),
+                SizedBox(
+                  width: 5,
+                )
+              ],
+            ),
+          ],
+        )),
+        PostItemYujiJunior(),
+        PostItemYujiHigh(),
+        PostItemYujiCol(),
+        PostItemYujiCom(),
+      ],
+    ));
+  }
+}
